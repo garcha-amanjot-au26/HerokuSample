f81612967d61119e22b08612bdb025785e5bee8f
amanjot-garcha-au19 <85173918+amanjot-garcha-au19@users.noreply.github.com>
2022-03-30 18:20:58 -0600

    CC Week 31 Day01

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const postData = require('./mock/posts.json');
+
+const port = 3002;
+
+app.get('/', (req,res) => {
+    res.send('hello')
+})
+
+app.get('/posts',(req,res) => {
+    res.json(postData.posts)
+})
+
+app.listen(port,console.log('Server live on :', port ));
